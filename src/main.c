78f557a005d5f61be7de52e40d87b73026732894
Mike Gran <spk121@yahoo.com>
2011-06-22 08:44:17 -0700

    Modify initialization to do global init and cleanup
    * src/main.c (cl_init): modified

--- src/main.c
+++ src/main.c
@@ -16,11 +16,16 @@ void
 cl_init ()
 {
   CURLcode ret;
-
-  cl_init_type ();
-  cl_init_const ();
-  cl_init_func ();
   
   ret = curl_global_init (CURL_GLOBAL_ALL);
-
+  if (ret)
+    scm_misc_error ("curl initialization", "initialization failed", SCM_EOL);
+  else
+    {
+      cl_init_type ();
+      cl_init_const ();
+      cl_init_func ();
+      
+      atexit (curl_global_cleanup);
+    }
 }
