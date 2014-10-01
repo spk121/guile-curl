960a735b2010807e0b972225f20f7452e7ca1da9
Mike Gran <spk121@yahoo.com>
2014-09-30 20:30:14 -0700

    Upgrade to GPL3 to match rest of code
    * test/lib.scm: moved to GPL3

--- test/lib.scm
+++ test/lib.scm
@@ -1,9 +1,9 @@
-;;;; test-suite/lib.scm --- generic support for testing
-;;;; Copyright (C) 1999, 2000, 2001, 2004, 2006, 2007 Free Software Foundation, Inc.
+;;;; test/lib.scm --- generic support for testing
+;;;; Copyright (C) 1999, 2000, 2001, 2004, 2006, 2007, 2014 Free Software Foundation, Inc.
 ;;;;
 ;;;; This program is free software; you can redistribute it and/or modify
 ;;;; it under the terms of the GNU General Public License as published by
-;;;; the Free Software Foundation; either version 2, or (at your option)
+;;;; the Free Software Foundation; either version 3, or (at your option)
 ;;;; any later version.
 ;;;;
 ;;;; This program is distributed in the hope that it will be useful,
