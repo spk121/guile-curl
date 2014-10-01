6a2771b9d2b86d509268f754cb862f0365a20d23
Mike Gran <spk121@yahoo.com>
2014-10-01 09:38:04 -0700

    Document  the encoding problems of HTTP requests
    * doc/guile-curl.texi: updated

--- cfg.mk
+++ cfg.mk
@@ -4,18 +4,18 @@
 #
 # This file is part of guile-curl
 #
-# guile-aspell is free software; you can redistribute it and/or modify it
+# guile-curl is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
 # any later version.
 #
-# guile-aspell is distributed in the hope that it will be useful, but
+# guile-curl is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with guile-aspell; see the file COPYING.  If not, write to the
+# along with guile-curl; see the file COPYING.  If not, write to the
 # Free Software Foundation, Fifth Floor, 51 Franklin Street, Boston,
 # MA 02111-1301, USA.
 
